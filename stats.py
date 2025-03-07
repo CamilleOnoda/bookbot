1a0eb44e2d922dcb1cd4ceed344a52ab515d36d2
Camille Onoda <info@camilleonoda.com>
2025-03-07 10:48:34 +0900

    add function that handles counting the number of times each character appears in a given string

--- stats.py
+++ stats.py
@@ -4,3 +4,17 @@ def count_num_words(text):
     for word in words:
         total_words += 1
     return total_words
+
+
+def count_each_character(text):
+    character_count = {}
+    lower_text = text.lower()
+
+    for char in lower_text:
+        if char.isalpha():
+            if char not in character_count:
+                character_count[char] = 1
+            elif char in character_count:
+                character_count[char] += 1
+    
+    return character_count
