d21968f48b5d00fa82012d83d518dd56fc29f244
Camille Onoda <info@camilleonoda.com>
2025-03-07 10:37:16 +0900

    use sys module to check arguments passed in. Update the text being printed

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+from stats import count_num_words
+import sys
+
+
 def main():
-    book_path = "/home/kamillentee/workspace/github.com/CamilleOnoda/bookbot/books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = open_text(book_path)
     num_words = count_num_words(text)
     characters_dict = count_each_character(text)
     sorted_dict = sort_dict(characters_dict)
 
-
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{num_words} words found in the document")
-    print()
+    print("============ BOOKBOT ============")
+    print(f"--- Analyzing book found at {book_path} ---")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     for key, value in sorted_dict.items():
-        print(f"The '{key}' character was found {value} times")
+        print(f"{key}: {value}")
     print()
     print("--- End of report ---")
 
@@ -18,14 +26,6 @@ def main():
 def open_text(path):
     with open(path) as f:
         return f.read()
-
-
-def count_num_words(text):
-    words = text.split()
-    total_words = 0
-    for word in words:
-        total_words += 1
-    return total_words
 
 
 def count_each_character(text):
